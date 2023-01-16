e1ab4392bc21bb2f7a6c8b7f38adf12a55202f63
froppy4 <paulkrucylak@gmail.com>
2023-01-15 21:15:44 -0600

    complete exercises flawlessly

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split("");
+    let revStrArray = []
+    for (let i = 0; i < strArray.length; i++){
+        revStrArray[i] =  strArray[strArray.length-1-i];
+    }
+    let revString = revStrArray.join("")
+    return revString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let filteredArray = array;
+    for (arg of args){
+        filteredArray = filteredArray.filter(function(x) {
+            return x !== arg;
+        })
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
